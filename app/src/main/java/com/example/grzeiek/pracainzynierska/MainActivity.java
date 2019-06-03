ec4d79cca876091ebf435297b70e09af2f1fd068
Bary <grzesiussj@gmail.com>
2019-06-04 00:38:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/grzeiek/pracainzynierska/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.grzeiek.pracainzynierska;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.rupins.drawercardbehaviour.CardDrawerLayout;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private CardDrawerLayout drawer;
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+
+        //loading the default fragment
+        loadFragment(new HomeFragment());
+
+        //getting bottom navigation view and attaching the listener
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+
+
+        Toolbar toolbar =  findViewById(R.id.toolbar);
+        setSupportActionBar( toolbar );
+
+        //Settings toolbar menu
+        drawer = (CardDrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.bringToFront();
+
+
+        drawer.setViewScale(Gravity.START, 0.8f);
+        drawer.setRadius(Gravity.START, 25);
+        drawer.setViewElevation(Gravity.START, 30);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected( @NonNull MenuItem item ) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.navigation_medications:
+                fragment = new MedicationsFragment();
+                break;
+
+            case R.id.navigation_prescriptions:
+                fragment = new PrescriptionsFragment();
+                break;
+
+            case R.id.navigation_history:
+                fragment = new HistoryFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen( GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
